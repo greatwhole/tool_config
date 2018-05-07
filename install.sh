8805b3f6bc5078a3c281e81de5beeb4d021152ae
huangx <greatwhole@greatwhole90.com>
2018-05-07 15:03:45 +0800

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,5 +1,18 @@
+# oh-my-zsh
+# https://github.com/robbyrussell/oh-my-zsh
 
 # tmux config
-# https://gist.github.com/simme/1297707
+# cat <<EOF >> ~/.tmux.conf
+# set -g mouse on
+# EOF
 
 brew install trash
+
+# pyenv
+# https://github.com/pyenv/pyenv#basic-github-checkout
+brew install pyenv
+echo -e 'if command -v pyenv 1>/dev/null 2>&1; then\n  eval "$(pyenv init -)"\nfi' >> ~/.zsh_profile
+
+brew install pyenv-virtualenv
+
+echo -e '\neval "$(pyenv init -)" \neval "$(pyenv virtualenv-init -)"' >> ~/.zsh_profile
